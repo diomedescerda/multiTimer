fcc89a643091a0c3dcef2fa0e0986c6bceb7d73b
diomedescerda <kenmatwofour@gmail.com>
2025-06-28 16:47:55 -0500

    feat: basic countdown

--- src/countdown.c
+++ src/countdown.c
@@ -1,25 +1,51 @@
 #include "../include/modules.h"
+#include <regex.h>
 #include <stdio.h>
+#include <string.h>
 #include <time.h>
 
 int countDownMain() {
-  int sec = 0;
-  printf("Seconds: ");
-  scanf("%d", &sec);
+  char name[256];
+  char time[9];
+  char subject[25];
+  regex_t regex;
+  int timeValidation;
+  int globalSeconds = 0;
+  timeValidation =
+      regcomp(&regex, "^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$",
+              REG_EXTENDED);
 
-  while (sec > 0) {
-    int h = sec / 3600;
-    int m = (sec % 3600) / 60;
-    int s = sec % 60;
+  do {
+    printf("Set countdown time (HH:MM:SS format): ");
+    fgets(time, sizeof(time), stdin);
+    time[strcspn(time, "\n")] = 0;
 
-    printf("\r%02d:%02d:%02d", h, m, s);
+    timeValidation = regexec(&regex, time, 0, NULL, 0);
+    if (timeValidation == REG_NOMATCH) {
+      printf("Invalid time format. Please try again (HH:MM:SS format).\n");
+    }
+  } while (timeValidation == REG_NOMATCH);
+  regfree(&regex);
+
+  int hour, min, sec;
+  if (sscanf(time, "%d:%d:%d", &hour, &min, &sec) != 3)
+    return -1;
+
+  globalSeconds = sec + min * 60 + hour * 3600;
+
+  while (globalSeconds > 0) {
+    hour = globalSeconds / 3600;
+    min = (globalSeconds % 3600) / 60;
+    sec = globalSeconds % 60;
+
+    printf("\r%02d:%02d:%02d", hour, min, sec);
     fflush(stdout);
 
     clock_t stop = clock() + CLOCKS_PER_SEC;
     while (clock() < stop) {
     }
 
-    sec--;
+    globalSeconds--;
   }
 
   printf("\rTime's up!\n");
